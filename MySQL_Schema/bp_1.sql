5b75ab77f9104d37a14c5a23bafcc3eb9a51d4a3
kennethchuson <kennethchuson@yahoo.com>
2023-02-10 14:29:56 -0800

    first commit

---
+++ MySQL_Schema/bp_1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    password VARCHAR(255) NOT NULL
+);

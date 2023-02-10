2e33bebdae13d81c00a2d7b5631b612120ab4f50
kennethchuson <kennethchuson@yahoo.com>
2023-02-10 14:31:35 -0800

    first change

--- MySQL_Schema/bp_1.sql
+++ MySQL_Schema/bp_1.sql
@@ -1,3 +1,9 @@
+
+/*
+Create a mysql schema for user that takes first name, last name, username, created_at, and password. 
+
+*/
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
